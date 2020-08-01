a17e86d0c71633e96ac8ad9718d397eb16c0a582
Adrian Romero <radrian031613@gmail.com>
2020-07-31 17:02:44 -0700

    changed naming confention for files and way nodemon opens files. images now render in ui after upload is clicked.

--- client/src/components/fileUpload.js
+++ client/src/components/fileUpload.js
@@ -1,31 +1,84 @@
 import React, { Fragment, useState } from 'react'
 
+// to make request bring in axios
+import axios from 'axios';
+
 const FileUpload = () => {
+//
+const [file, setFile] = useState('');
+const [fileName, setFileName] = useState('Choose File');
+// state for uploaded fime
+const [uploadedFile, setUploadedFile] = useState({}); // object being sent to server fileName: file.name
+
+// set up on change event
+const onChange = e => {
+  setFile(e.target.files[0]); // zero since only one file will be chosen
+  setFileName(e.target.files[0].name);
+};
+
+// set up on submit
+const onSubmit = async e => {
+  e.preventDefault();
+  const formData = new FormData();
+  formData.append('file', file); // this is req.files.file (from backend)
+  // wrap async await axios function in try catch
+
+  try {
+    const res = await axios.post('/upload', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+
+    const { fileName, filePath } = res.data;
+
+    setUploadedFile({ fileName, filePath });
+  } catch (err) {
+    if(err.response.satus === 500) {
+      console.log('there was a problem with server');
+    } else {
+      console.log(err);
+    }
+  }
+};
+
 
 
 
   return (
     <Fragment>
-      <form>
+      <form onSubmit={onSubmit}>
 
         <div className='custom-file mb-4'>
           <input
           type='file'
           className='custom-file-input'
           id='customFile'
+          onChange={onChange}
           />
           <label className='custom-file-label' htmlFor='customFile'>
-            Choose file
+            {fileName} 
           </label>
         </div>
         <input 
         type='submit' 
         value='Upload'
-        className='btn btnprimary btn-block mt-4'
+        className='btn btn-primary btn-block mt-4'
         />
       </form>
+      {/* conditional ternary */}
+      {uploadedFile ? (
+      <div className='row mt-5'>
+        <div className='col-md-6 m-auto'>
+          <h3 className='text-center'>
+            { uploadedFile.fileName }
+          </h3>
+          <img src={uploadedFile.filePath} style={{ width: '100%' }} alt=''/>
+        </div>
+      </div> 
+      ) : null} 
     </Fragment>
-  )
-}
+  );
+};
 
 export default FileUpload;
--- server.js
+++ server.js
@@ -1,31 +1,26 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 
-// initialize express
 const app = express();
 
 app.use(fileUpload());
 
-
-// this would be in routes normally
+// Upload Endpoint
 app.post('/upload', (req, res) => {
-  if(req.files === null) {
-    return res.status(400).json({ msg: 'No file found' });
-
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });
   }
 
-  // variable for file in req.files
   const file = req.files.file;
 
   file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
-    if(err) {
+    if (err) {
       console.error(err);
       return res.status(500).send(err);
     }
 
-    // if no errors
-    res.json({ fileName: file.name, filePath: `/uploads${file.name}` });
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
   });
 });
 
-app.listen(5000, () => console.log('Server started...'));
+app.listen(5000, () => console.log('Server Started...'));
